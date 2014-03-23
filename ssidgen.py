43013f5de54f85fa45350e65f48457c044123f24
Bastien Faure <bastien@faure.io>
2014-03-23 20:41:09 +0100

    Apparence and modules

---
+++ ssidgen.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import sys
+import argparse
+
+print '+----------------------------------+'
+print '|    ESSID Dictionary Generator    |'
+print '+----------------------------------+'
+print
