d06a40788c13c6714453dad1d1d2103909e493b8
Bastien Faure <bastien@faure.io>
2014-03-24 14:50:08 +0100

    Modular length for suffix

--- ssidgen.py
+++ ssidgen.py
@@ -12,14 +12,19 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-c","--company", help="The company name")
 parser.add_argument("-f","--fai", help="A known FAI")
 parser.add_argument("-o","--output", help="the output file")
+parser.add_argument("-l","--length", help="use this option in combination with --fai", default=2)
 args = parser.parse_args()
 
 company = args.company
 fai     = args.fai
 output  = args.output
+length  = args.length
 
 if fai and company:
     sys.exit("You cannot specifiy both -c and -f parameters")
+elif not fai and not company:
+    parser.print_help()
+    sys.exit()
 
 def bruteforcize(string):
     return [string, string.upper(), string[0].upper()+string[1:]]
@@ -28,9 +33,11 @@ dictionnary = open(output + '.txt' if output else 'ssid.txt', "w")
 
 
 if fai:
-    for i in range(65535):
+    for i in range(pow(2,8*int(length))):
         dictionnary.write(fai + '_%04x\n' % i)
         dictionnary.write(fai + '_%04X\n' % i)
+        dictionnary.write(fai + '-%04x\n' % i)
+        dictionnary.write(fai + '-%04X\n' % i)
 
 if company:
     prefix = 'wifi'
